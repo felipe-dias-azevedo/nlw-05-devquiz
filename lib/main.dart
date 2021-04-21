da52498e90c0639796d61be71a6175d654197f08
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2021-04-20 22:35:09 -0300

    aula 1 finalizada

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ;
+//   }
+// }
